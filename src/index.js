b6bb700982a348bbb90a6094d7d7c3841decae26
imadhq <74244566+imadhq@users.noreply.github.com>
2024-03-07 11:04:26 +0000

    Change file structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import './styles.css';
+import './styles/styles.css';
+import sampleImage from './assets/images/SampleIcon.png';
+
+const img = new Image();
+img.src = sampleImage;
+document.body.appendChild(img);
 
 console.log ('test')
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
@@ -17,6 +18,43 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              outputPath: 'assets/images',
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets/fonts',
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ico)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets/icons',
+              name: '[name].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
