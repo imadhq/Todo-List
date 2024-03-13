f4785fda9e1301947620967b293bbc815e4fa56e
imadhq <74244566+imadhq@users.noreply.github.com>
2024-03-13 13:21:08 +0000

    Ability to add new task or project

--- src/index.js
+++ src/index.js
@@ -102,6 +102,12 @@ const openEditModal = (task) => {
     modal.close();
     renderTaskList();
   });
+
+  const cancelButton = modal.querySelector('.cancel-button');
+  cancelButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    modal.close();
+  });
 }
 
 const createModal = (task) => {
@@ -167,6 +173,135 @@ function clearInputFields() {
   dueDate.value = '';
   lowPriority.checked = true;
 }
+
+const createNewTaskModal = (() => {
+  const modal  = document.createElement('dialog');
+  modal.classList.add('new-task-modal');
+  modal.classList.add('jsmodal');
+
+  modal.innerHTML = `
+  <form>
+    <p><label for="task-title">Title:</label></p>
+    <input type="text" id="task-title" class="text-input">
+
+    <p><label for="task-description">Description:</label></p>
+    <input type="text" id="task-description" class="text-input">
+
+    <p><label for="due-date">Due Date:</label></p>
+    <input type="date" id="due-date" class="due-date-input">
+
+    <p>Priority:</p>
+    <label>
+      Low
+      <input type="radio" name="priority" id="low-priority-radio" value="low" checked}> 
+    </label>
+
+    <label>
+      Medium
+      <input type="radio" name="priority" id="medium-priority-radio" value="medium"> 
+    </label>
+
+    <label>
+      High
+      <input type="radio" name="priority" id="high-priority-radio" value="high"> 
+    </label>
+
+    <p class="dialog-button-section">
+    <button class="cancel-button dialog-btn">Cancel</button>
+    <button class="js-add-task dialog-btn">Save</button>
+    </p>
+  </form>
+  `
+  document.body.appendChild(modal);
+})();
+
+const addNewTaskButton = (() => {
+  const button = document.createElement('button');
+  button.classList.add('new-task-button');
+  button.textContent = 'New Task';
+  const header = document.querySelector('.title-container');
+  header.appendChild(button);
+
+  button.addEventListener('click', () => {
+    const modal = document.querySelector('.new-task-modal');
+    modal.showModal();
+  })
+})();
+
+const newTaskModal = document.querySelector('.new-task-modal');
+const newTaskSaveButton = newTaskModal.querySelector('.js-add-task');
+newTaskSaveButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const modal = newTaskModal;
+  const title = modal.querySelector('#task-title').value;
+  const description = modal.querySelector('#task-description').value;
+  const dueDate = modal.querySelector('#due-date').value;
+  const lowPriority = modal.querySelector('#low-priority-radio');
+  const mediumPriority = modal.querySelector('#medium-priority-radio');
+  const highPriority = modal.querySelector('#high-priority-radio');
+  let selectedRadio;
+
+  if (lowPriority.checked) {
+    selectedRadio = 'low';
+  } else if (mediumPriority.checked) {
+    selectedRadio = 'medium';
+  } else if (highPriority.checked) {
+    selectedRadio = 'high';
+  }
+
+  const newTask = new Task(title, description, dueDate, selectedRadio);
+  currentProject.taskList.push(newTask);
+  renderTaskList();
+  modal.close();
+  clearInputFields();
+})
+
+const allModal = document.querySelector('.jsmodal');
+const cancelButton = allModal.querySelector('.cancel-button');
+cancelButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  allModal.close();
+})
+
+const createNewProjectModal = (() => {
+  const modal  = document.createElement('dialog');
+  modal.classList.add('new-project-modal');
+
+  modal.innerHTML = `
+    <p><label for="project-title">New Project Title:</label></p>
+    <input type="text" id="project-title" class="text-input">
+
+    <p class="dialog-button-section">
+    <button class="cancel-button dialog-btn">Cancel</button>
+    <button class="js-add-project dialog-btn">Save</button>
+    </p>
+  `
+  document.body.appendChild(modal);
+
+  modal.querySelector('.js-add-project')
+    .addEventListener('click', () => {
+      const name = modal.querySelector('#project-title').value;
+      const newProject = projectManager.createProject(name);
+      currentProject = newProject;
+      renderProjectList();
+      renderTaskList();
+      modal.close();
+    })
+})();
+
+const addNewProject = (() => {
+  const button = document.createElement('button');
+  button.classList.add('new-project-button');
+  button.textContent = 'New Project';
+  const header = document.querySelector('.title-container');
+  header.insertBefore(button, header.firstChild);
+
+  button.addEventListener('click', () => {
+    const modal = document.querySelector('.new-project-modal');
+    modal.showModal();
+  })
+})();
 
 renderTaskList();
 renderProjectList();
